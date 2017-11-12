010436b5a35243e7f244e3a9bf604d3dabb0b7a1
Kachin <kachin0725@gmail.com>
2017-11-12 02:56:09 +0000

    automatically backed up by learn

--- flow-control.js
+++ flow-control.js
@@ -1,8 +1,8 @@
 function basicTeenager(age)
-{if (13<=basicTeenager=<19){"You are a teenager!"}}
+{if (13<=age=<19){"You are a teenager!"}}
 
+basicTeenager=13
 
-basicTeenager(13)
 
 function teenager(age) {
 
